bdcac2208f574133059c8f265f40aa9970e35502
Brendan Chen <smallpigeon1@gmail.com>
2022-11-14 21:40:55 -0800

    add day02a

---
+++ day02a/src/main.rs
@@ -0,0 +1,27 @@
+use std::time::Instant;
+
+fn main() {
+    let now = Instant::now();
+
+    let input = include_str!("../input.txt");
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let iter = line.split('x');
+        // vectors slow, arrays fast
+        let mut d = [0, 0, 0];
+        let mut next = 0;
+        for i in iter {
+            d[next] = i.parse::<i32>().expect("Unknown input");
+            next += 1;
+        }
+
+        let min = (d[0] * d[1]).min(d[0] * d[2]).min(d[1] * d[2]);
+        sum += min + (2 * d[0] * d[1]) + (2 * d[0] * d[2]) + (2 * d[1] * d[2]);
+    }
+
+    println!("{sum}");
+
+    let elapsed = now.elapsed().as_micros();
+    println!("Time elapsed: {elapsed}");
+}
